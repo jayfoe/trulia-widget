bbc4fcde84abd473f58cf0b660a35f26f04cc19f
jonathan <jonford3@hotmail.com>
2017-07-02 01:36:14 -0700

    set state for data

--- src/components/listings-hub.js
+++ src/components/listings-hub.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class ListingsHub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      batmanData: {},
+      supermanData: {}
+    }
+  }
 
   componentDidMount() {
     axios.get('/data/batmanRealty.json')
@@ -9,6 +16,7 @@ class ListingsHub extends Component {
         let str = response.data;
         let cleaned = str.substring(str.lastIndexOf("=")+1,str.lastIndexOf(";"));
         let batmanData = JSON.parse(cleaned);
+        this.setState({ batmanData });
       })
 
     axios.get('/data/supermanRealty.json')
@@ -16,6 +24,7 @@ class ListingsHub extends Component {
         let str = response.data;
         let cleaned = str.substring(str.lastIndexOf("=")+1,str.lastIndexOf(";"));
         let supermanData = JSON.parse(cleaned);
+        this.setState({ supermanData });
       })
   }
 
