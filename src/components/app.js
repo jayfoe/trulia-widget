9291e658e9922cb836867ddb79f490c88825c292
jonathan <jonford3@hotmail.com>
2017-07-02 01:25:48 -0700

    moved axios.get out of app into  listings-hub

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,9 @@
 import React from 'react';
-import axios from 'axios';
 import ButtonsGroup from './buttons-group';
 import ListingsHub from './listings-hub';
 
 class App extends React.Component {
   render() {
-    axios.get('/data/batmanRealty.json')
-      .then((response) => {
-        let str = response.data;
-        let cleaned = str.substring(str.lastIndexOf("=")+1,str.lastIndexOf(";"));
-        let batmanData = JSON.parse(cleaned);
-      })
-
-    axios.get('/data/supermanRealty.json')
-      .then((response) => {
-        let str = response.data;
-        let cleaned = str.substring(str.lastIndexOf("=")+1,str.lastIndexOf(";"));
-        let supermanData = JSON.parse(cleaned);
-      })
-      
     return (
       <div>
         <h4>Awesome Listings Widget</h4>
--- src/components/listings-hub.js
+++ src/components/listings-hub.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ListingsHub extends Component {
+
+  componentDidMount() {
+    axios.get('/data/batmanRealty.json')
+      .then((response) => {
+        let str = response.data;
+        let cleaned = str.substring(str.lastIndexOf("=")+1,str.lastIndexOf(";"));
+        let batmanData = JSON.parse(cleaned);
+      })
+
+    axios.get('/data/supermanRealty.json')
+      .then((response) => {
+        let str = response.data;
+        let cleaned = str.substring(str.lastIndexOf("=")+1,str.lastIndexOf(";"));
+        let supermanData = JSON.parse(cleaned);
+      })
+  }
+
   render() {
     return (
       <div></div>
