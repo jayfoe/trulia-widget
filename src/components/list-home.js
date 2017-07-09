0b192df8c18e4cdea3bf487172228892e46c7575
jonathan <jonford3@hotmail.com>
2017-07-09 02:01:31 -0700

    conditional rendering done

--- src/components/list-home.js
+++ src/components/list-home.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react';
 import { Col, Thumbnail, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 const ListHome = ({home}) => {
+  
+
   return (
     <ListGroupItem>
       <a href={home.url}>
         <Image src={home.img} href={home.url} alt="242x200" responsive />
-        <h4>{home.address}</h4>
+        {home.address && <h4>{home.address}</h4>}
       </a>
       <div>
-        <h3>${home.price}</h3>
-        <p>{home.beds} beds</p>
-        <p>{home.baths} baths</p>
-        <p>{home.sqft} sq ft</p>
-        <p>Built in {home.built}</p>
+        {home.price && <h3>${home.price}</h3>}
+        {home.beds && <p>{home.beds} beds</p>}
+        {home.baths && <p>{home.baths} baths</p>}
+        {home.sqft && <p>{home.sqft} sq ft</p>}
+        {home.built && <p>Built in {home.built}</p>}
       </div>
     </ListGroupItem>
   )
