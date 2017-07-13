15ef36de99710cf147ebd59b8efc9489a28e8386
jonathan <jonford3@hotmail.com>
2017-07-12 21:17:37 -0700

    changed port value back

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
-let port = process.env.PORT || 3000);
 app.use('/', express.static('public'));
 app.use('/static', express.static('dist'));
-app.listen(port, function() {
-  console.log('App is running on port: ' + port);
-});
+app.listen(process.env.PORT || 3000);
