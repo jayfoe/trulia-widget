6be41f4f8e47454fb342226c06fb2b98d4e468ff
jonathan <jonford3@hotmail.com>
2017-07-12 22:15:29 -0700

    node port

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use('/', express.static('public'));
 app.use('/static', express.static('dist'));
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT);
+});
