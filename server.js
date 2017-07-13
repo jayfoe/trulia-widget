09793eb82b5686c5770dd4efaa0fbe0200e7329b
jonathan <jonford3@hotmail.com>
2017-07-12 21:05:17 -0700

    server port change

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+let port = process.env.PORT || 3000);
 app.use('/', express.static('public'));
 app.use('/static', express.static('dist'));
-app.listen(process.env.PORT || 3000);
+app.listen(port, function() {
+  console.log('App is running on port: ' + port);
+});
